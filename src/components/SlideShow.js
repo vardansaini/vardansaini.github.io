bb60e9c28eb0519786c9e5c5b50ce9e029c11558
vardansaini <vardan1@ualberta.ca>
2023-04-08 12:58:34 -0600

    video update

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,50 +1,73 @@
-import React from 'react'
-import { Button } from './Button'
-import './HeroSection.css'
-import VS from './images/VS_new.jpg'
-import { Carousel } from 'react-bootstrap';
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import VS from "./images/VS_new.jpg";
+import { Carousel } from "react-bootstrap";
 
 function SlideShow() {
-    return (
-        // <div className='hero-container'>
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img
-                        className="d-block w-100"
-                        src='images/Inclusify.png'
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={500}>
-                    <img
-                        className="d-block w-100"
-                        src='images/H-Log.jpg'
-                        alt="Second slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src='images/H-Log.jpg'
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-            // </div>
+  return (
+    // <div className='hero-container'>
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <div className="video-responsive">
+          <iframe
+            width="853"
+            height="480"
+            src={"https://www.youtube.com/embed/y9NM_rabQCU"}
+            //   src="https://www.youtube.com/embed/sqlaWFy-ywQ"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        </div>
+        <Carousel.Caption>
+          <div style={{ color: "black " }}>
+            <h3>Social Distribution</h3>
+            <p>An open source analysis tool available on Github</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+      <div className="video-responsive">
+          <iframe
+            width="853"
+            height="480"
+            src={"https://www.youtube.com/embed/sqlaWFy-ywQ"}
+            //   src="https://www.youtube.com/embed/sqlaWFy-ywQ"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        </div>
+        <Carousel.Caption>
+        <div style={{ color: "black " }}>
+            <h3>Capstone Dashboard</h3>
+            <p>An open source analysis tool available on Github</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="images/H-Log.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    // </div>
+  );
 
-    )
+  // YoutubeEmbed.propTypes = {
+  //   embedId: PropTypes.string.isRequired
+  // };
 }
 
-export default SlideShow
+export default SlideShow;
